b4f40bcd320ceff339e31b8d3e750cae9aad240a
sjhxbug <sjhxbug1024@gmail.com>
2019-11-19 18:58:59 -0600

    testbenching finished

--- Verilog/data_memory.v
+++ Verilog/data_memory.v
@@ -1,6 +1,6 @@
 // 32bits MIPS Processor Data Memory
 
-module data_memory(MemWrite, WriteData, ALUresult, MemRead, clk, data_result);
+module data_memory(MemWrite, WriteData, ALUresult, MemRead, clk, data_result,reset);
 
 input MemWrite;// memory write signal
 input MemRead;	// memory read signal
@@ -8,10 +8,10 @@ input [31:0] ALUresult;// ALU result address
 input [31:0] WriteData;	// Data need to be write 
 input clk;
 input reset;// reset signal for clearing memory
-output reg [15:0] data_result;	// data result
+output wire [31:0] data_result;	// data result
 
 integer i;
-	reg [31:0] ram [255:0]；//init a 16*65025 memory
+	reg [31:0] ram [255:0]; //init a 16*65025 memory
 	wire [7:0] ram_addr = ALUresult[7:0]; //get memory address
 	initial
 	begin
@@ -28,5 +28,5 @@ begin
 end
 assign data_result = (MemRead==1'b1)?ram[ram_addr] : 32'h0;
 
-endmodule;
+endmodule
 
--- Verilog/data_memory_tb.v
+++ Verilog/data_memory_tb.v
@@ -23,6 +23,7 @@ data_memory test(
 initial begin
 		//save 1 to F[0]
 	clk=1;
+	
 	MemRead = 0;
 	MemWrite = 1;
 	ALUresult = 32'h10010000;
@@ -50,25 +51,175 @@ initial begin
 	MemRead = 0;
 	MemWrite = 1;
 	ALUresult = 32'h10010008;
-	WriteData = 32'h00000002;;
+	WriteData = 32'h00000002;
 	#10;
 	//read 1 from F[1]
 	MemRead = 1;
 	MemWrite = 0;
 	ALUresult = 32'h10010004;
-	WriteData = 32'h00000000;;
+	WriteData = 32'h00000000;
 	#10;
 	//read 2 from F[2]
 	MemRead = 1;
 	MemWrite = 0;
 	ALUresult = 32'h10010008;
-	WriteData = 32'h00000000;;
+	WriteData = 32'h00000000;
 	#10;
 	//write 3 into F[3]
 	MemRead = 0;
 	MemWrite = 1;
 	ALUresult = 32'h1001000C;
-	WriteData = 32'h00000000;;
+	WriteData = 32'h00000003;
+	#10;
+	//read 2 from F[2]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult = 32'h10010008;
+	WriteData = 32'h00000000;
+	#10;
+	// read 3 from F[3]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult = 32'h10010010;
+	WriteData = 32'h00000000;
+	#10;
+	// write 5 into F[4]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult = 32'h10010010;
+	WriteData = 32'h00000005;
+	#10;
+	// read 3 from F[3]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult= 32'h1001000C;
+	WriteData= 32'h00000000;
+	#10;
+	// read 5 from F[4]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010010;
+	WriteData=32'h00000000;
+	#10;
+	// write 8 into F[5]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h10010014;
+	WriteData=32'h00000008;
+	#10;
+	//read 5 from F[4]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010010;
+	WriteData=32'h00000000;
+	#10;
+	//read 8 from F[5]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010014;
+	WriteData=32'h00000000;
+	#10;
+	//write 13 into F[6]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h10010018;
+	WriteData=32'h0000000D;
+	#10;
+	//read 8 from F[5]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010014;
+	WriteData=32'h00000000;
+	#10;
+	//read 13 from F[6]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010018;
+	WriteData=32'h00000000;
+	#10;
+	//write 21 into F[7]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h1001001C;
+	WriteData=32'h00000015;
+	#10;
+	//read 13 from F[6]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010018;
+	WriteData=32'h00000000;
+	#10;
+	//read 21 from F[7]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h1001001C;
+	WriteData=32'h00000000;
+	#10;
+	//write 34 into F[8]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h10010020;
+	WriteData=32'h00000022;
+	#10;
+	//read 21 from F[7]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h1001001C;
+	WriteData=32'h00000000;
+	#10;
+	//read 34 from F[8]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010020;
+	WriteData=32'h00000000;
+	#10;
+	//write 55 into F[9]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h10010024;
+	WriteData=32'h00000037;
+	#10;
+	//read 34 from F[8]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010020;
+	WriteData=32'h00000000;
+	#10;
+	// read 55 from F[9]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010024;
+	WriteData=32'h00000000;
+	#10;
+	// write 89 into F[10]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h10010028;
+	WriteData=32'h00000059;
+	#10;
+	// read 55 from F[9]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010024;
+	WriteData=32'h00000000;
+	#10;
+	// read 89 from F[10]
+	MemRead=1;
+	MemWrite=0;
+	ALUresult=32'h10010028;
+	WriteData=32'h00000000;
+	#10;
+	//write 144 into F[11]
+	MemRead=0;
+	MemWrite=1;
+	ALUresult=32'h1001002C;
+	WriteData=32'h00000090;
+	#10;
 	end
 	always #5 clk = !clk;
+	initial begin
+	$monitor("ALUresult=%d,data_result=%d\n",ALUresult,data_result);
+	#1000 $stop;
+	
+	end
 endmodule
